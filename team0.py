d881e54379a40d17c5e46b0a624ff2c68360628e
rmcfadden217 <mc1899ross@gmail.com>
2017-01-09 13:06:12 -0500

    More stuff

--- team0.py
+++ team0.py
@@ -17,6 +17,16 @@ def move(my_history, their_history, my_score, their_score):
     Make my move.
     Returns 'c' or 'b'. 
     '''
+    if len(my_history)==0: # It's the first round: collude
+        return 'b'
+    elif 'b' in their_history[-10:]:
+        return 'b'               
+    else:
+        if random.random()<0.1: 
+            return 'c'        
+        else:
+            return 'b'  
+        
 
     # my_history: a string with one letter (c or b) per round that has been played. with this opponent.
     # their_history: a string of the same length as history, possibly empty. 
@@ -25,8 +35,6 @@ def move(my_history, their_history, my_score, their_score):
     
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
-    
-    return 'c'
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
